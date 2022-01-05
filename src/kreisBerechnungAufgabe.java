a1d9094c4cbec35da9ff57b39214d27f84fb6fe3
iekorkmaz <ibrahim-eren.korkmaz@ibykus.de>
2022-01-05 14:14:22 +0100

    -start

---
+++ src/kreisBerechnungAufgabe.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class kreisBerechnungAufgabe {
+    public static void main(String[] args) {
+//        final  double pi = 3.14159265;
+        double pi = Math.PI;
+        Scanner durchmessereing = new Scanner(System.in);
+
+        System.out.print("Bitte Durchmesser eingeben: ");
+        double durchmesser = durchmessereing.nextDouble();
+        double ergebnis = pi*durchmesser/2*durchmesser/2;
+        System.out.print("Die Kreisfläche lautet: " + ergebnis);
+    }
+}
