ec53b540e6c111289fa30336a27f6076295e095c
iekorkmaz <ibrahim-eren.korkmaz@ibykus.de>
2022-01-05 14:18:54 +0100

    -Aufgabe Fertig

--- src/kreisBerechnungAufgabe.java
+++ src/kreisBerechnungAufgabe.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class kreisBerechnungAufgabe {
     public static void main(String[] args) {
 //        final  double pi = 3.14159265;
+        //wenn ich die PI-Zahl von Math,PI nehme, ist das Ergebnis genauer.
         double pi = Math.PI;
         Scanner durchmessereing = new Scanner(System.in);
 
