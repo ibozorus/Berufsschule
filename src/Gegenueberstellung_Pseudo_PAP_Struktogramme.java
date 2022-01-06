280f2fc8e47d5ad3cdb21e67ffae67050ab74447
iekorkmaz <ibrahim-eren.korkmaz@ibykus.de>
2022-01-06 12:06:55 +0100

    -Aufgabe Fertig

---
+++ src/Gegenueberstellung_Pseudo_PAP_Struktogramme.java
@@ -0,0 +1,40 @@
+public class Gegenueberstellung_Pseudo_PAP_Struktogramme {
+    public static void main(String[] args) {
+
+//        int x = 5;
+//        if((x == 5)){
+//            System.out.println("Bedingung A");
+//
+//        }
+//        else if(x==4){
+//
+//            System.out.println("Bedingung B");
+//        }
+//        else if(x==3){
+//
+//            System.out.println("Bedingung C");
+//        }
+//        else{
+//            System.out.println("Bedingung D");
+//        }
+//    }
+//        int x = 5;
+//    switch(x){
+//        case 5:System.out.println("Bedingung A");
+//        case 4:System.out.println("Bedingung B");
+//        case 3:System.out.println("Bedingung C");
+//        case 2:System.out.println("Bedingung D");
+//        default:System.out.println("Bedingung E");
+//
+//    }
+//    }
+        int x = 0;
+        while(true){
+            x++;
+            System.out.println(x+".mal");
+            if(x==10){
+                break;
+            }
+        }
+    }
+}
